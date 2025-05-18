186004c8d758db43ae10812a7f8cd9673b6cc392
Andre-Acosta2022 <luigui.acosta@upeu.edu.pe>
2025-05-18 15:51:34 -0500

    tercer commit

--- src/main/java/upeu/edu/pe/ms_paciente/repository/ContEmergenciaRepository.java
+++
@@ -1,7 +0,0 @@
-package upeu.edu.pe.ms_paciente.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import upeu.edu.pe.ms_paciente.domain.ContactoEmergencia;
-
-public interface ContEmergenciaRepository extends JpaRepository<ContactoEmergencia, Long>{
-
-}
